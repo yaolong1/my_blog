e365506f752109049b71c84342598b11a2af0a43
“yaolong <1638538651@qq.com>
2019-08-06 19:32:56 +0800

    bootstrap

---
+++ my_blog/src/main/java/com/yaolong/my_blog/config/MyWebAppConfigurer.java
@@ -0,0 +1,10 @@
+package com.yaolong.my_blog.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author yaoLong
+ * @date 2019/8/6  14:33
+ */
+public class MyWebAppConfigurer extends WebMvcConfigurerAdapter {
+}
