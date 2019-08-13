042741391eaa4408b7bbc89f1dd335f3d1852a6e
“wyaolong <1638538651@qq.com>
2019-08-13 11:24:58 +0800

    html

--- my_blog/src/test/java/com/yaolong/my_blog/MyBlogApplicationTests.java
+++ my_blog/src/test/java/com/yaolong/my_blog/MyBlogApplicationTests.java
@@ -1,16 +1,146 @@
 package com.yaolong.my_blog;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.model.OSSObject;
+import com.aliyun.oss.model.OSSObjectSummary;
+import com.aliyun.oss.model.ObjectListing;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.*;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MyBlogApplicationTests {
-
+    /**
+     * 列举文件
+     */
     @Test
-    public void contextLoads() {
+    public void ossListFile() {
+        // Endpoint以杭州为例，其它Region请按实际情况填写。
+        String endpoint = "http://oss-cn-beijing.aliyuncs.com";
+// 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
+        String accessKeyId = "LTAIWwjaWzuiJWpW";
+        String accessKeySecret = "faXlw0CwoAAMHJkvVimDQVqhVRnjLt";
+        String bucketName = "yaolong";
+
+// 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+// ossClient.listObjects返回ObjectListing实例，包含此次listObject请求的返回结果。
+        ObjectListing objectListing = ossClient.listObjects(bucketName);
+// objectListing.getObjectSummaries获取所有文件的描述信息。
+        for (OSSObjectSummary objectSummary : objectListing.getObjectSummaries()) {
+            System.out.println(" - " + objectSummary.getKey() + "  " +
+                    "(size = " + objectSummary.getSize() + ")");
+        }
+
+// 关闭OSSClient。
+        ossClient.shutdown();
     }
 
+    /**
+     * 创建存储空间
+     */
+    @Test
+    public void ossCreateBucket() {
+        // Endpoint以杭州为例，其它Region请按实际情况填写。
+        String endpoint = "http://oss-cn-beijing.aliyuncs.com";
+// 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
+        String accessKeyId = "LTAIWwjaWzuiJWpW";
+        String accessKeySecret = "faXlw0CwoAAMHJkvVimDQVqhVRnjLt";
+        String bucketName = "yaolong1";
+
+// 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+// 创建存储空间。
+        ossClient.createBucket(bucketName);
+
+// 关闭OSSClient。
+        ossClient.shutdown();
+
+    }
+
+    /**
+     * 上传文件
+     */
+    @Test
+    public void ossUpload(){
+        // Endpoint以杭州为例，其它Region请按实际情况填写。
+        String endpoint = "http://oss-cn-beijing.aliyuncs.com";
+// 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
+        String accessKeyId = "LTAIWwjaWzuiJWpW";
+        String accessKeySecret = "faXlw0CwoAAMHJkvVimDQVqhVRnjLt";
+        String bucketName = "yaolong";
+        String objectName = "yaolong";
+
+// 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+// 上传内容到指定的存储空间（bucketName）并保存为指定的文件名称（objectName）。
+        String content = "Hello OSS";
+        ossClient.putObject(bucketName, objectName, new ByteArrayInputStream(content.getBytes()));
+
+// 关闭OSSClient。
+        ossClient.shutdown();
+    }
+
+    /**
+     *下载文件
+     */
+    @Test
+    public void ossDownload() throws IOException {
+        // Endpoint以杭州为例，其它Region请按实际情况填写。
+        String endpoint = "http://oss-cn-beijing.aliyuncs.com";
+// 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
+        String accessKeyId = "LTAIWwjaWzuiJWpW";
+        String accessKeySecret = "faXlw0CwoAAMHJkvVimDQVqhVRnjLt";
+        String bucketName = "yaolong";
+        String objectName = "yaolong";
+
+// 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+// 调用ossClient.getObject返回一个OSSObject实例，该实例包含文件内容及文件元信息。
+        OSSObject ossObject = ossClient.getObject(bucketName, objectName);
+// 调用ossObject.getObjectContent获取文件输入流，可读取此输入流获取其内容。
+        InputStream content = ossObject.getObjectContent();
+        if (content != null) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+            while (true) {
+                String line = reader.readLine();
+                if (line == null) break;
+                System.out.println("\n" + line);
+            }
+            // 数据读取完成后，获取的流必须关闭，否则会造成连接泄漏，导致请求无连接可用，程序无法正常工作。
+            content.close();
+        }
+
+// 关闭OSSClient。
+        ossClient.shutdown();
+
+    }
+    @Test
+    public void ossDelete(){
+        // Endpoint以杭州为例，其它Region请按实际情况填写。
+        String endpoint = "http://oss-cn-beijing.aliyuncs.com";
+// 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
+        String accessKeyId = "LTAIWwjaWzuiJWpW";
+        String accessKeySecret = "faXlw0CwoAAMHJkvVimDQVqhVRnjLt";
+        String bucketName = "yaolong";
+        String objectName = "yaolong";
+
+// 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+// 删除文件。
+        ossClient.deleteObject(bucketName, objectName);
+
+// 关闭OSSClient。
+        ossClient.shutdown();
+    }
 }
